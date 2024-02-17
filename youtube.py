0123665674fefced23d08d97fbaf3afca68b25df
Gowri-29 <157974205+Gowri-29@users.noreply.github.com>
2024-02-17 16:44:21 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -182,6 +182,22 @@ if Channel_data:  # Check if Channel_data list is not empty
 else:
     Channel_DF = pd.DataFrame()  # Create an empty DataFrame if no data is available
 
+myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
+cur = myconnection.cursor()
+drop_query = " drop table if exists Channel"
+cur.execute(drop_query)
+columns = ", ".join(
+    f"{column_name} {dtype}"
+    for column_name, dtype in zip(Channel_DF.columns, Channel_DF.dtypes))
+
+sql_create_table = f"CREATE TABLE IF NOT EXISTS Channel ({columns});"
+Channel = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
+cur.execute("create table Channel(Channel_Name text,Channel_Id text,Subscription_Count int,Channel_Views int,Channel_Description text, Playlist_Id text, Total_Video int)")
+sql = "insert into Channel values(%s,%s,%s,%s,%s,%s,%s)"
+
+for i in range(0,len(Channel_DF)):
+    cur.execute(sql,tuple(Channel_DF.iloc[i]))
+    myconnection.commit()
 
 #creating playlistDF    
 
@@ -193,6 +209,23 @@ if Playlist_data:  # Check if Playlist_data list is not empty
 else:
     Playlist_DF = pd.DataFrame()  # Create an empty DataFrame if no data is available
 
+myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
+cur = myconnection.cursor()
+drop_query = " drop table if exists Playlist"
+cur.execute(drop_query)
+
+columns = ", ".join(
+    f"{column_name} {dtype}"
+    for column_name, dtype in zip(Playlist_DF.columns, Playlist_DF.dtypes))
+
+sql_create_table = f"CREATE TABLE IF NOT EXISTS Playlist ({columns});"
+Playlist = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
+cur.execute("create table Playlist(Playlist_Id text,Channel_Id text,Title text,Channel_Name text,PublishedAt text, Video_Count int)")
+sql = "insert into Playlist values(%s,%s,%s,%s,%s,%s)"
+
+for i in range(0,len(Playlist_DF)):
+    cur.execute(sql,tuple(Playlist_DF.iloc[i]))
+    myconnection.commit()
 
 
 #creating commentsDF
@@ -205,6 +238,22 @@ if Comments_data:  # Check if Comments_data list is not empty
 else:
     Comments_DF = pd.DataFrame()  # Create an empty DataFrame if no data is available
 
+myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
+cur = myconnection.cursor()
+drop_query = " drop table if exists Comments"
+cur.execute(drop_query)
+columns = ", ".join(
+    f"{column_name} {dtype}"
+    for column_name, dtype in zip(Comments_DF.columns, Comments_DF.dtypes))
+
+sql_create_table = f"CREATE TABLE IF NOT EXISTS Comments ({columns});"
+Comments = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
+cur.execute("create table Comments(Comment_Id text,Video_Id text,Comments text,Comment_Author text,Comment_Published_Date text)")
+sql = "insert into Comments values(%s,%s,%s,%s,%s)"
+
+for i in range(0,len(Comments_DF)):
+    cur.execute(sql,tuple(Comments_DF.iloc[i]))
+    myconnection.commit()
 
 #creating videosDF
 for i in Channel.find({},{'_id':0,"Videos":1}):
@@ -217,140 +266,32 @@ if Videos_data:  # Check if Videos_data list is not empty
 else:
     Videos_DF = pd.DataFrame()  # Create an empty DataFrame if no data is available
 
+myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
+cur = myconnection.cursor()
+drop_query = " drop table if exists Videos"
+cur.execute(drop_query)
 
+columns = ", ".join(
+    f"{column_name} {dtype}"
+    for column_name, dtype in zip(Videos_DF.columns, Videos_DF.dtypes))
 
-def Creating_Cha_Table_and_Uploading():
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-    drop_query = " drop table if exists Channel"
-    cur.execute(drop_query)
-    columns = ", ".join(
-        f"{column_name} {dtype}"
-        for column_name, dtype in zip(Channel_DF.columns, Channel_DF.dtypes))
+sql_create_table = f"CREATE TABLE IF NOT EXISTS Videos ({columns});"
+Videos = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
+cur.execute("create table Videos(Channel_Name text,Channel_Id text,Video_Id text,Title text,Tags text,Description text,Published_Date text,Duration text,Views int,Thumbnail text,Comments int,Likes int,Dislikes int,Favorite_Count int,Definition text,Caption_Status text)")
+sql = "insert into Videos values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-    sql_create_table = f"CREATE TABLE IF NOT EXISTS Channel ({columns});"
-    Channel = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
-    cur.execute("create table Channel(Channel_Name text,Channel_Id text,Subscription_Count int,Channel_Views int,Channel_Description text, Playlist_Id text, Total_Video int)")
-    sql = "insert into Channel values(%s,%s,%s,%s,%s,%s,%s)"
-
-    for i in range(0,len(Channel_DF)):
-        cur.execute(sql,tuple(Channel_DF.iloc[i]))
-        myconnection.commit()
-
-
-def Creating_Pla_Table_and_Uploading(): 
-            
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-    drop_query = " drop table if exists Playlist"
-    cur.execute(drop_query)
-
-    columns = ", ".join(
-        f"{column_name} {dtype}"
-        for column_name, dtype in zip(Playlist_DF.columns, Playlist_DF.dtypes))
-
-    sql_create_table = f"CREATE TABLE IF NOT EXISTS Playlist ({columns});"
-    Playlist = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
-    cur.execute("create table Playlist(Playlist_Id text,Channel_Id text,Title text,Channel_Name text,PublishedAt text, Video_Count int)")
-    sql = "insert into Playlist values(%s,%s,%s,%s,%s,%s)"
-
-    for i in range(0,len(Playlist_DF)):
-        cur.execute(sql,tuple(Playlist_DF.iloc[i]))
-        myconnection.commit()
-
-def Creating_Com_Table_and_Uploading():
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-    drop_query = " drop table if exists Comments"
-    cur.execute(drop_query)
-    columns = ", ".join(
-        f"{column_name} {dtype}"
-        for column_name, dtype in zip(Comments_DF.columns, Comments_DF.dtypes))
-
-    sql_create_table = f"CREATE TABLE IF NOT EXISTS Comments ({columns});"
-    Comments = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
-    cur.execute("create table Comments(Comment_Id text,Video_Id text,Comments text,Comment_Author text,Comment_Published_Date text)")
-    sql = "insert into Comments values(%s,%s,%s,%s,%s)"
-
-    for i in range(0,len(Comments_DF)):
-        cur.execute(sql,tuple(Comments_DF.iloc[i]))
-        myconnection.commit()
-
-def Creating_Vid_Table_and_Uploading():
-
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-    drop_query = " drop table if exists Videos"
-    cur.execute(drop_query)
-
-    columns = ", ".join(
-        f"{column_name} {dtype}"
-        for column_name, dtype in zip(Videos_DF.columns, Videos_DF.dtypes))
-
-    sql_create_table = f"CREATE TABLE IF NOT EXISTS Videos ({columns});"
-    Videos = sql_create_table.replace("float64","float").replace("category","text").replace("int64","int")
-    cur.execute("create table Videos(Channel_Name text,Channel_Id text,Video_Id text,Title text,Tags text,Description text,Published_Date text,Duration text,Views int,Thumbnail text,Comments int,Likes int,Dislikes int,Favorite_Count int,Definition text,Caption_Status text)")
-    sql = "insert into Videos values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-
-    for i in range(0,len(Videos_DF)):
-        cur.execute(sql,tuple(Videos_DF.iloc[i]))
-        myconnection.commit()
-
-
-def Creating_Table_and_Uploading_SQL():
-    Creating_Cha_Table_and_Uploading()
-    Creating_Pla_Table_and_Uploading()
-    Creating_Com_Table_and_Uploading()
-    Creating_Vid_Table_and_Uploading()
-
-#streamlit displaying table:    
-
-def show_Channel():
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-
-    channel_sql = pd.read_sql_query('SELECT * FROM channel', myconnection)
-
-# Display the data in a Streamlit table
-    st.table(channel_sql)
-
-
-def show_Playlist():
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-
-    Playlist_Sql=pd.read_sql_query('select * from Playlist',myconnection)
-    st.table(Playlist_Sql)   
-
-
-def show_Comments():
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-
-    Comments_Sql=pd.read_sql_query('select * from Comments',myconnection)
-    st.table(Comments_Sql)
-
-def show_Videos():
-    myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
-    cur = myconnection.cursor()
-
-    Videos_Sql=pd.read_sql_query('select * from videos',myconnection)
-    st.table(Videos_Sql)
+for i in range(0,len(Videos_DF)):
+    cur.execute(sql,tuple(Videos_DF.iloc[i]))
+    myconnection.commit()
 
 
 #steamlit 
 
-with st.sidebar:
-    st.title(":blue[YOUTUDE DATA HARVESTING AND WAREHOUSE PROJECT]")
-    st.header(":violet[PROJECT METHODOLOGY]")
-    st.caption(":red[Data Collection from Youtube (Python,API Integration)]")
-    st.caption(":red[Data Storage (Mongodb)]")
-    st.caption(":red[Data Transfer (Mongodb to Sql)]")
-    st.caption(":red[Data Analysis (Sql)]")
+st.title(":blue[YOUTUBE DATA HARVESTING AND WAREHOUSE PROJECT]")
+    
+Channel_Id = st.text_input(":violet[Enter the Youtube Channel Id(e.g: UCHE7CG-****************)]")
 
-Channel_Id = st.text_input("Enter the Channel Id(e.g: UCHE7CG-****************)")
-
-if st.button("Collect and Store data"):
+if st.button(":green[Extraction and Importing]"):
     Channel_ids=[]
     mydb = client['Youtube_Data']
     Channel=mydb["Channel"]
@@ -361,36 +302,14 @@ if st.button("Collect and Store data"):
         st.success("Channel Details already exists")
     else:
         insert= Channel_input(Channel_Id)
-        st.success("Uploading Channel Details Successful")
-    
-
-if st.button("Migrate to Sql"):
-    Table = Creating_Table_and_Uploading_SQL()
-    st.success("Migration Successful")
-
-Show_Table=st.radio("SELECT THE TABLE FOR VIEW",("CHANNELS","PLAYLISTS","COMMENTS","VIDEOS"))
-
-try:
-    if Show_Table == "CHANNELS":
-        show_Channel()
-
-    elif Show_Table == "PLAYLISTS":
-        show_Playlist()
-
-    elif Show_Table =="COMMENTS":
-        show_Comments()
-
-    elif Show_Table =="VIDEOS":
-        show_Videos()
-
-except:
-    st.warning(":red[No data available]")
+        st.success("Channel Details uploaded")
+        
+                
 #SQL CONNECTIONS:
 myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
 cur = myconnection.cursor()
 
-
-question = st.selectbox("Select Your Questions",("1. What are the names of all the videos and their corresponding channels?",
+Show_Table=st.selectbox(":bold[SELECT THE QUESTIONS FOR VIEW]",("1. What are the names of all the videos and their corresponding channels?",
                                                 "2. Which channels have the most number of videos, and how many videos do they have?",
                                                 "3. What are the top 10 most viewed videos and their respective channels?",
                                                 "4. How many comments were made on each video, and what are their corresponding video names?",
@@ -401,7 +320,8 @@ question = st.selectbox("Select Your Questions",("1. What are the names of all t
                                                 "9. What is the average duration of all videos in each channel, and what are their corresponding channel names?",
                                                 "10. Which videos have the highest number of comments, and what are their corresponding channel names?"))
 
-if question =="1. What are the names of all the videos and their corresponding channels?":
+
+if Show_Table =="1. What are the names of all the videos and their corresponding channels?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
     try:
@@ -411,7 +331,7 @@ if question =="1. What are the names of all the videos and their corresponding c
         st.warning("No data available")    
 
 
-elif question=="2. Which channels have the most number of videos, and how many videos do they have?":
+elif Show_Table =="2. Which channels have the most number of videos, and how many videos do they have?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
     try:
@@ -420,7 +340,7 @@ elif question=="2. Which channels have the most number of videos, and how many v
     except:    
         st.warning("No data available")
 
-elif question=="3. What are the top 10 most viewed videos and their respective channels?":
+elif Show_Table =="3. What are the top 10 most viewed videos and their respective channels?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -431,7 +351,7 @@ elif question=="3. What are the top 10 most viewed videos and their respective c
         st.warning("No data available") 
 
 
-elif question=="4. How many comments were made on each video, and what are their corresponding video names?":
+elif Show_Table =="4. How many comments were made on each video, and what are their corresponding video names?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -442,7 +362,7 @@ elif question=="4. How many comments were made on each video, and what are their
         st.warning("No data available")  
 
 
-elif question=="5. Which videos have the highest number of likes, and what are their corresponding channel names?":
+elif Show_Table =="5. Which videos have the highest number of likes, and what are their corresponding channel names?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -452,7 +372,7 @@ elif question=="5. Which videos have the highest number of likes, and what are t
     except:
         st.warning("No data available")       
 
-elif question=="6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?":
+elif Show_Table =="6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -463,7 +383,7 @@ elif question=="6. What is the total number of likes and dislikes for each video
         st.warning("No data available")
 
 
-elif question=="7. What is the total number of views for each channel, and what are their corresponding channel names?":
+elif Show_Table =="7. What is the total number of views for each channel, and what are their corresponding channel names?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -473,7 +393,7 @@ elif question=="7. What is the total number of views for each channel, and what 
     except:
         st.warning("No data available") 
 
-elif question=="8. What are the names of all the channels that have published videos in the year 2022?":
+elif Show_Table =="8. What are the names of all the channels that have published videos in the year 2022?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -485,7 +405,7 @@ elif question=="8. What are the names of all the channels that have published vi
         st.warning("No data available") 
 
 
-elif question=="9. What is the average duration of all videos in each channel, and what are their corresponding channel names?":
+elif Show_Table =="9. What is the average duration of all videos in each channel, and what are their corresponding channel names?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
@@ -498,7 +418,7 @@ elif question=="9. What is the average duration of all videos in each channel, a
     except:
         st.warning("No data available")       
 
-elif question=="10. Which videos have the highest number of comments, and what are their corresponding channel names?":
+elif Show_Table =="10. Which videos have the highest number of comments, and what are their corresponding channel names?":
     myconnection = pymysql.connect(host='127.0.0.1',user='root',passwd='gowri@2903',database="Youtube_Data")
     cur = myconnection.cursor()
 
